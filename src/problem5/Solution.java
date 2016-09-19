53a51f7ddc34036aa5dd9c12bcaf0572c45c1dd9
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-09-19 20:06:09 +0800

    2016.9.19第三题已解决

---
+++ src/problem5/Solution.java
@@ -0,0 +1,32 @@
+package problem5;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution {
+	public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		Stack<Integer> s = new Stack();
+		while (listNode != null) {
+			s.push(listNode.val);
+			listNode = listNode.next;
+		}
+		while (!s.isEmpty()) {
+			al.add(s.pop());
+		}
+		return al;
+	}
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next = null;
+
+	ListNode(int val) {
+		this.val = val;
+	}
+}
