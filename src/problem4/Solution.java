3a56c93c7a6bb5b69e1edbe7ac9bc40dff9a488a
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-09-19 19:32:12 +0800

    2016.9.12第二题已解决

---
+++ src/problem4/Solution.java
@@ -0,0 +1,24 @@
+package problem4;
+
+public class Solution {
+public static String replaceSpace(StringBuffer str) {
+
+		StringBuffer sb = new StringBuffer();
+		for(int x=0;x<str.length();x++)
+		{
+			if(str.charAt(x)==' ')
+			{
+				sb.append("%20");
+			}
+			else
+				sb.append(str.charAt(x));
+		}
+		
+    	return sb.toString();
+    }
+	public static void main(String [] args)
+	{
+		
+		System.out.println(replaceSpace(new StringBuffer("We Are Happy.")));
+	}
+}
