a8c5576d626b61123d3168a276513bf6e24bdb27
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-09-20 22:36:09 +0800

    2016.9.20未完成，找时间弄懂。。。

--- src/problem6/Solution.java
+++ src/problem6/Solution.java
@@ -2,8 +2,29 @@ package problem6;
 
 
 public class Solution {
+public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        TreeNode root= new TreeNode(pre[0]);
+	return root;
+    }
+	public int findRoot(int [] in,int rootval)
+	{
+		for(int x=0;x<in.length;x++)
+		{
+			if(in[x]==rootval)
+				return x;
+		}
+		return -1;
+	}
 	public static void main(String [] args)
 	{
 		System.out.println();
 	}
 }
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+ 
