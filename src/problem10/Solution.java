b5e8e3d33010f8e705085b1a50480e6b415b5af8
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-09-24 13:41:27 +0800

    2016.9.24已完成

---
+++ src/problem10/Solution.java
@@ -0,0 +1,16 @@
+package problem10;
+
+public class Solution {
+	public static int NumberOf1(int n) {
+		int count = 0;
+		while (n != 0) {
+			n = n & (n - 1);
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(NumberOf1(1));
+	}
+}
