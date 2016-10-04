a451832f59c59b3c1ca654d7c22e02756c3a8085
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-10-04 16:42:31 +0800

    2016.9.29补交

---
+++ src/problem16/Solution.java
@@ -0,0 +1,14 @@
+package problem16;
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
