7fe3fbf3a1095bccc0b27ddff5bd15f2d4cc1ab5
81248397@qq.com <zhangxu@zhangxudeMacBook-Pro.local>
2016-10-04 19:38:06 +0800

    2016.9.30补交

--- src/problem16/Solution.java
+++ src/problem16/Solution.java
@@ -1,8 +1,34 @@
 package problem16;
+
+
 public class Solution {
-    public ListNode ReverseList(ListNode head) {
+    public static ListNode ReverseList(ListNode head) {
+    	if(head==null)
+    		return null;
+    	ListNode root=head;
+    	ListNode now=head;
+    	ListNode now_next=head.next;
+    	head.next=null;
+    	
+    	while(now_next!=null)
+    	{
+    		now=now_next;
+    		now_next=now_next.next;
+    		now.next=root;
+    		root=now;
+    	}
+		head.next=null;
+    	return root;
 
     }
+    public static void main(String[] args)
+	{
+		ListNode head=new ListNode(1);
+	
+		
+		head=ReverseList(head);
+		System.out.println(head);
+	}
 }
 class ListNode {
     int val;
